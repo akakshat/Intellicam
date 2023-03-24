d5a76e0c81c64cc6d802fd5c1ac29d5af8a6ad22
Akshat Garg <akasatgarg@gmail.com>
2023-03-24 22:31:41 +0530

    Integrating Backend to Frontend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,20 +18,69 @@ const CreatePost = () => {
       prompt: '',
       photo: '',
     }
-  );
+);
 
-  const [generatingImg, setgeneratingImg] = useState(false);
-  const [loading, setloading] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-const generateImage = () =>{
+const generateImage = async () =>{
+  if(form.prompt){
+    try {
+      setGeneratingImg(true);
+      const response = await fetch('http://localhost:8081/api/v1/intel', 
+      {
+        method: 'POST',
+        headers: 
+        {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: form.prompt }),
+    }) 
+      const data = await response.json();
 
+      setForm({ ...form, photo: `data:image/jpeg;base64, ${data.photo}` });
+    } 
+    catch (error) 
+    {
+      alert(error);
+    } 
+    finally 
+    {
+      setGeneratingImg(false);
+    }
+  } 
+  else 
+  {
+    alert('Please provide proper prompt');
+  }
+  };
 
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-const handleSubmit = () =>{
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8081/api/v1/intel', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
 
-
-}
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
 const handleChange = (e) => 
   setForm({ ...form, [e.target.name]: e.target.value });
--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/', async(req, res) => {
 const startServer = async () => {
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server started on port http://localhost:8080'))
+        app.listen(8081, () => console.log('Server started on port http://localhost:8081'))
     } catch (error) {
         console.log(error);
     }
